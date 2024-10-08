7fa8507a491af2da15199abd27f72bf067a7ef25
Дмитрий <artemovdmitriy@mail.ru>
2024-10-08 14:37:10 +0300

    Первая программа

---
+++ FirstApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string MyName = "Dmitriy";
+            byte MyAge = 33;
+            bool HaveIApet = false;
+            int MyShoeSize = 41;
+
+            Console.WriteLine("My name is " + MyName);
+            Console.WriteLine("My age is " + MyAge);
+            Console.WriteLine("Do I have a pet? " + HaveIApet);
+            Console.WriteLine("My shoe size is " + MyShoeSize);
+
+            Console.ReadKey();
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,20 @@
-﻿using System;
-
-namespace FirstApp
+﻿namespace FirstApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string MyName = "Dmitriy";
+            decimal MyAge = 33;
+            bool HaveIApet = false;
+            int MyShoesSize = 41;
+
+            Console.WriteLine("My name is" + MyName);
+            Console.WriteLine("My age is" + MyAge);
+            Console.WriteLine("Do I have a pet?" + HaveIApet);
+            Console.WriteLine("My shoe size is" + MyShoesSize);
+
+            Console.ReadKey();
         }
     }
 }
